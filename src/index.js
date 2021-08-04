e15345772a4b6c007ff46563223cd50717a20955
nikeusov <nikotin331765115@gmail.com>
2021-08-04 19:43:34 +0300

    feat: complete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+    const arr = [
+        'zero', 'one', 'free', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    const tens = [
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    let str = "";
+    let num = 0;
+
+    if (number >= 100) {
+        num = (number / 100) | 0;
+        str = `${arr[num]} hundred`;
+        number -= num * 100;
+    }
+    if (number > 19) {
+        num = (number / 10) | 0;
+        number -= num * 10;
+        str = `${str} ${tens[num]} ${arr[number]}`;
+    } else {
+        str = `${str} ${arr[number]}`;
+    }
+    return str.trim();
 }
