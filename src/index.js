6d13f92177d664a80612202e20349893d24cf757
nikeusov <nikotin331765115@gmail.com>
2021-08-06 14:11:08 +0300

    fix: replace function body

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
     const arr = [
-        'zero', 'one', 'free', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
     ];
     const tens = [
-        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+        'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
     ];
 
     let str = "";
     let num = 0;
 
-    if (number >= 100) {
-        num = (number / 100) | 0;
-        str = `${arr[num]} hundred`;
-        number -= num * 100;
+    if (number === 0) return 'zero';
+
+    if (number >= 1000) {
+        num = (number / 1000) | 0; // получаем количество тысяч
+        str = `${arr[num]} thousand`;   // вставляем в строку кол тысяч
+        number -= num * 1000;   // отсекаем тысячи
     }
-    if (number > 19) {
+
+    if (number >= 100) {
+        num = (number / 100) | 0; // получаем количество сотен
+        str = `${str} ${arr[num]} hundred`;   // вставляем в строку кол сотен
+        number -= num * 100;   // отсекаем сотни
+    }
+   
+    if (number > 19) {        
         num = (number / 10) | 0;
         number -= num * 10;
         str = `${str} ${tens[num]} ${arr[number]}`;
     } else {
         str = `${str} ${arr[number]}`;
     }
+
     return str.trim();
 }
